71e060078814467544e3a50ce5d88879f4a0fdab
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-09-25 01:14:03 +0530

    feat: server side rendering

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,55 +1,47 @@
-"use client";
 import { Navbar, Footer } from "@/components";
-import { useEffect, useState } from "react";
-import {Post, PostProps} from '../../../components/post';
+import { Post, PostProps } from '../../../components/post';
 
-
-
-export default function Page({ params }: { params: { id: string } }) {
-
+export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
 
-  const [post, setPost] = useState<PostProps>({} as PostProps);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  let post: PostProps | null = null;
+  let error: string | null = null;
 
-  const loadData = async (id: string) => {
-      try {
-          const res = await fetch(`https://account.bytlabs.co/api/v1/post/${id}`);
-          const data = await res.json();
-          console.log(data);
-          setPost(data);
-          setLoading(false);
-      } catch (err: any) {
-          setError(err.message);
-          setLoading(false);
-      }
-  };
+  try {
+    const res = await fetch(`https://account.bytlabs.co/api/v1/post/${id}`, {
+      cache: 'no-store',
+    });
+    
+    if (!res.ok) {
+      throw new Error("Post not found");
+    }
 
-  useEffect(() => {
-    loadData(id);
-  }, [id]);
-
+    post = await res.json();
+  } catch (err: any) {
+    error = err.message;
+  }
 
   const Loading = () => (
     <div className="flex justify-center items-center min-h-screen">
       <div className="loader"></div>
-    </div>)
+    </div>
+  );
 
-const Error = () => (
+  const Error = () => (
     <div className="relative min-h-screen w-full">
-        <Post 
+      <Post 
         title="404 Not Found" 
         summary="Oops! The post you're looking for isn't available. Stay updated with the latest news and articles by downloading our app."
         coverImagePath="https://play-lh.googleusercontent.com/BSkud_lo8xYZ8bFSxSYcrE3I_CipymjqWPpQR3A5CUDwJpMjQiCoXZXKpTabcsI5cvU=w3840-h2160-rw"
-        />
-      </div>)
+      />
+    </div>
+  );
 
   return (
     <>
       <Navbar />
       <div className="relative min-h-screen w-full">
-        {loading ? <Loading /> : error? <Error/> : <Post {...post} />}
+        {error ? <Error /> : post ? <Post {...post} /> : <Loading />}
       </div>
       <Footer />
     </>
