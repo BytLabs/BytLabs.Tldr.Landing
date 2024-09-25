55205b00869ad62039b7a6d27e9ed503bd976b80
IamRaviMaurya <82931953+IamRaviMaurya@users.noreply.github.com>
2024-09-26 01:39:58 +0530

    feat: fixed build issue

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     });
     
     if (!res.ok) {
-      throw new Error("Post not found");
+     error = "Post not found";
     }
 
     post = await res.json();
